c7cd63d72077150fd3f5e31200eee6c483a4f03d
Aditi-dev-28 <aditiajith2811@gmail.com>
2025-01-30 08:22:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-This is lakshmy's first commit
+var http = require('http')
+
+var port = 8080
+
+var server = http.createServer(function (request, response) {
+
+  console.log('New request: ' + request.url)  
+
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.end('Hello World\n')
+})
+
+server.listen(port, "0.0.0.0")
+
+console.log('Server running at http://localhost:' + port)
